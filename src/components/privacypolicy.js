58272ab9b51149c7bd4eefca8925987baa67c40e
https://github.com/jawadgujjar/bmh.git <https://github.com/jawadgujjar/bmh.git>
2024-10-05 05:02:04 +0500

    complete frntend

--- src/components/privacypolicy.js
+++ src/components/privacypolicy.js
@@ -5,7 +5,7 @@ function Privacypolicy1() {
   return (
     <div className="privacy-policy-container">
       <h1 className="policy-heading">Privacy Policy</h1>
-      <p>
+      <div className="privacy-p">
         Quality Woven Labels is committed to ensuring that your privacy is
         protected. Should we ask you to provide certain information by which
         you can be identified when using this website, then you can be assured
@@ -13,41 +13,41 @@ function Privacypolicy1() {
         Quality Woven Labels may change this policy from time to time by
         updating this page. You should check this page from time to time to
         ensure that you are happy with any changes.
-      </p>
+      </div>
       <h2 className="policy-subheading">What we collect</h2>
-      <p>We may collect the following information:</p>
-      <ul>
-        <li>name</li>
-        <li>contact information including email address</li>
-        <li>demographic information such as postcode, preferences, and interests</li>
-        <li>other information relevant to customer surveys and/or offers</li>
-        <li>your IP address</li>
+      <div className="privacy-p">We may collect the following information:</div>
+      <ul className="privacy-ul">
+        <li className="privacy-li">name</li>
+        <li className="privacy-li">contact information including email address</li>
+        <li className="privacy-li">demographic information such as postcode, preferences, and interests</li>
+        <li className="privacy-li">other information relevant to customer surveys and/or offers</li>
+        <li className="privacy-li">your IP address</li>
       </ul>
-      <p>
+      <div className="privacy-p">
         For the exhaustive list of cookies we collect see the List of cookies
         we collect section.
-      </p>
+      </div>
       <h2 className="policy-subheading">What we do with the information we gather</h2>
-      <p>
+      <div className="privacy-p">
         We require this information to understand your needs and provide you with a better
         service, and in particular for the following reasons:
-      </p>
-      <ul>
-        <li>Internal record keeping.</li>
-        <li>We may use the information to improve our products and services.</li>
-        <li>We may periodically send promotional emails about new products, special offers, or other information that we think you may find interesting using the email address you have provided.</li>
-        <li>We may also use your information to contact you regarding any quotations placed through our website.</li>
-        <li>We may contact you by email, phone, fax, or mail.</li>
+      </div>
+      <ul className="privacy-ul">
+        <li className="privacy-li">Internal record keeping.</li>
+        <li className="privacy-li">We may use the information to improve our products and services.</li>
+        <li className="privacy-li">We may periodically send promotional emails about new products, special offers, or other information that we think you may find interesting using the email address you have provided.</li>
+        <li className="privacy-li">We may also use your information to contact you regarding any quotations placed through our website.</li>
+        <li className="privacy-li">We may contact you by email, phone, fax, or mail.</li>
       </ul>
       <h2 className="policy-subheading">Security</h2>
-      <p>
+      <div className="privacy-p">
         We are committed to ensuring that your information is secure. In order
         to prevent unauthorized access or disclosure, we have put in place
         suitable physical, electronic, and managerial procedures to safeguard
         and secure the information we collect online.
-      </p>
+      </div>
       <h2 className="policy-subheading">How we use cookies</h2>
-      <p>
+      <div className="privacy-p">
         A cookie is a small file which asks permission to be placed on your
         computer's hard drive. Once you agree, the file is added and the
         cookie helps analyse web traffic or lets you know when you visit a
@@ -58,18 +58,20 @@ function Privacypolicy1() {
         pages are being used. This helps us analyse data about web page
         traffic and improve our website in order to tailor it to customer
         needs. We only use this information for statistical analysis purposes
-        and then the data is removed from the system.<p>Overall, cookies help us
-        provide you with a better website, by enabling us to monitor which
-        pages you find useful and which you do not. A cookie in no way gives
-        us access to your computer or any information about you, other than
-        the data you choose to share with us. You can choose to accept or
-        decline cookies. Most web browsers automatically accept cookies, but
-        you can usually modify your browser setting to decline cookies if you
-        prefer. This may prevent you from taking full advantage of the
-        website.</p> 
-      </p>
+        and then the data is removed from the system.
+        <div className="privacy-p">
+          Overall, cookies help us provide you with a better website, by enabling us to monitor which
+          pages you find useful and which you do not. A cookie in no way gives
+          us access to your computer or any information about you, other than
+          the data you choose to share with us. You can choose to accept or
+          decline cookies. Most web browsers automatically accept cookies, but
+          you can usually modify your browser setting to decline cookies if you
+          prefer. This may prevent you from taking full advantage of the
+          website.
+        </div>
+      </div>
       <h2 className="policy-subheading">Links to other websites</h2>
-      <p>
+      <div className="privacy-p">
         Our website may contain links to other websites of interest. However,
         once you have used these links to leave our site, you should note that
         we do not have any control over that other website. Therefore, we
@@ -78,19 +80,19 @@ function Privacypolicy1() {
         sites are not governed by this privacy statement. You should exercise
         caution and look at the privacy statement applicable to the website in
         question.
-      </p>
+      </div>
       <h2 className="policy-subheading">Controlling your personal information</h2>
-      <p>
+      <div className="privacy-p">
         You may choose to restrict the collection or use of your personal
         information in the following ways:
-      </p>
-      <ul>
-        <li>
+      </div>
+      <ul className="privacy-ul">
+        <li className="privacy-li">
           Whenever you are asked to fill in a form on the website, look for
           the box that you can click to indicate that you do not want the
           information to be used by anybody for direct marketing purposes.
         </li>
-        <li>
+        <li className="privacy-li">
           If you have previously agreed to us using your personal information
           for direct marketing purposes, you may change your mind at any time
           by writing to or emailing us at{" "}
@@ -100,19 +102,21 @@ function Privacypolicy1() {
           .
         </li>
       </ul>
-      <p>
+      <div className="privacy-p">
         We will not sell, distribute or lease your personal information to
         third parties unless we are required by law to do so. You may request
         details of personal information which we hold about you under the Data
         Protection Act 1998. A small fee will be payable. If you would like a
         copy of the information held on you please write to 1760 Glasco
-        Turnpike Woodstock, NY 12498. <p>If you believe that any information we
-        are holding on you is incorrect or incomplete, please write to or
-        email us as soon as possible, at the above address. We will promptly
-        correct any information found to be incorrect.</p>
-      </p>
+        Turnpike Woodstock, NY 12498. 
+        <div className="privacy-p">
+          If you believe that any information we are holding on you is incorrect or incomplete, please write to or
+          email us as soon as possible, at the above address. We will promptly
+          correct any information found to be incorrect.
+        </div>
+      </div>
       <h2 className="policy-subheading">Return Policy</h2>
-      <p>
+      <div className="privacy-p">
         Returns will be accepted within 30 days of receiving your order. You
         are responsible for return shipping if your labels match your Digital
         Proof. If your labels do not match your Digital Proof we will pay the
@@ -121,7 +125,7 @@ function Privacypolicy1() {
         card, your refund will be credited back to your card. Paypal orders
         will receive a PayPal refund. Orders paid for by e-check will have
         their bank account credited.
-      </p>
+      </div>
     </div>
   );
 }
